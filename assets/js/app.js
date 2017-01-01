b6d8afdc916712aeae8eabae6566f941b7ad65d1
jacobproffer <jacobhardingproffer@gmail.com>
2017-01-01 15:48:55 -0500

    Work on script for movie-tickets (First commit of 2017)

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,40 @@ var movies = {
     {
       "title": "alien",
       "genre": "sci-fi",
-      "year-watched": 1979,
-      "theatre-watched": "gkc"
+      "yearWatched": 1979,
+      "theatreWatched": "gkc"
+    },
+    {
+      "title": "aliens",
+      "genre": "sci-fi",
+      "yearWatched": 1986,
+      "theatreWatched": "gkc"
+    },
+    {
+      "title": "alien 3",
+      "genre": "sci-fi",
+      "yearWatched": 1993,
+      "theatreWatched": "gkc"
+    },
+    {
+      "title": "alien: resurrection",
+      "genre": "sci-fi",
+      "yearWatched": 1997,
+      "theatreWatched": "gkc"
     }
   ]
 };
+
+for (var i = 0, l = movies.movies.length; i < l; i++) {
+  // Get movie values
+  var movieTitle = movies.movies[i].title;
+  var movieYear = movies.movies[i].yearWatched;
+  var movieTheatre = movies.movies[i].theatreWatched;
+  // Append ticket-row div to movie-tickets
+  var movieTickets = document.querySelector('.movie-tickets');
+  var ticketRow = document.createElement('div');
+  ticketRow.className = 'ticket-row';
+  var movieEntry = document.createElement('div');
+  movieTickets.appendChild(ticketRow);
+  // Append movie data
+}
