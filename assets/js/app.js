83d1d08f09cc405abfad2742f42c53f24a0c9ac3
Jacob Proffer <jacobhardingproffer@gmail.com>
2017-01-01 17:55:49 -0500

    Update scripts

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,8 @@
-var movies = {
-  "movies": [
-    {
-      "title": "alien",
-      "genre": "sci-fi",
-      "yearWatched": 1979,
-      "theatreWatched": "gkc"
-    },
-    {
-      "title": "aliens",
-      "genre": "sci-fi",
-      "yearWatched": 1986,
-      "theatreWatched": "gkc"
-    },
-    {
-      "title": "alien 3",
-      "genre": "sci-fi",
-      "yearWatched": 1993,
-      "theatreWatched": "gkc"
-    },
-    {
-      "title": "alien: resurrection",
-      "genre": "sci-fi",
-      "yearWatched": 1997,
-      "theatreWatched": "gkc"
-    }
-  ]
-};
+// I'd prefer to do this with vanilla JavaScript
+// I'd prefer to sort year by XX/XX/XXXX instead of XXXX
 
-for (var i = 0, l = movies.movies.length; i < l; i++) {
-  // Get movie values
-  var movieTitle = movies.movies[i].title;
-  var movieYear = movies.movies[i].yearWatched;
-  var movieTheatre = movies.movies[i].theatreWatched;
-  // Append ticket-row div to movie-tickets
-  var movieTickets = document.querySelector('.movie-tickets');
-  var ticketRow = document.createElement('div');
-  ticketRow.className = 'ticket-row';
-  var movieEntry = document.createElement('div');
-  movieTickets.appendChild(ticketRow);
-  // Append movie data
-}
+var $movies = $('.movie-tickets');
+$movies.find('.ticket-row').sort(function(a, b) {
+    return +a.dataset.year - +b.dataset.year;
+})
+.appendTo($movies);
