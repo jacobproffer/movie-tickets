da35af482fe1694a2cbe1ef9946d285a18db5511
Jacob Proffer <jacobhardingproffer@gmail.com>
2017-01-01 21:21:57 -0500

    Add entries and clean js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,3 @@
-// I'd prefer to do this with vanilla JavaScript
-// I'd prefer to sort year by XX/XX/XXXX instead of XXXX
-
-// var $movies = $('.movie-tickets');
-// $movies.find('.ticket-row').sort(function(a, b) {
-//     return +a.dataset.year - +b.dataset.year;
-// })
-// .appendTo($movies);
-
 $(".movie-tickets .ticket-row").sort(function (a, b) {
     return new Date($(".date", a).data("date")) - new Date($(".date", b).data("date"));
 }).appendTo(".movie-tickets");
