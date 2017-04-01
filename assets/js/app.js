0f2d7c73de572adef1953490021c954ea5acf053
Jacob Proffer <jacobhardingproffer@gmail.com>
2017-03-31 23:11:21 -0400

    Begin work on data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,9 @@
 $(".movie-tickets .ticket-row").sort(function (a, b) {
     return new Date($(".date", a).data("date")) - new Date($(".date", b).data("date"));
 }).appendTo(".movie-tickets");
+
+var num = $(".ticket-row").length;
+var date = $(".info-col[data-theatre='thomas theatre group']").length;
+console.log(date);
+
+$(".numMovies h1").html(" " + num);
