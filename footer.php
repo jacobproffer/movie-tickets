5a333b072b3564129938325e9808af84baa6118e
jacobproffer <jacobhardingproffer@gmail.com>
2017-05-25 21:27:22 -0400

    Work on styles

--- footer.php
+++ footer.php
@@ -1,3 +1,8 @@
+      <footer>
+        <div class="container">
+          <span>?</span>
+        </div>
+      </footer>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script src="dist/js/app.min.js" charset="utf-8"></script>
