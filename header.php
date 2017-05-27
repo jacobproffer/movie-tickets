c3673095208b42a908a78d2ed82e9c9fb17f6730
jacobproffer <jacobhardingproffer@gmail.com>
2017-05-27 19:07:51 -0400

    Add GA

--- header.php
+++ header.php
@@ -25,6 +25,14 @@ $data = json_decode($movies_json, TRUE);
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200" rel="stylesheet">
     <link rel="stylesheet" href="dist/css/project-main.min.css">
   </head>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-100014360-1', 'auto');
+    ga('send', 'pageview');
+  </script>
   <body>
     <div class="info-modal close-info-modal">
       <div class="info-modal-container">
