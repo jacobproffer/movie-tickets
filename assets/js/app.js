8b0280b0339567c8957e8733ef49a901e887021a
jacobproffer <jacobhardingproffer@gmail.com>
2017-07-03 11:21:36 -0400

    Add modal fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,12 +6,14 @@ var num;
 $('.open-info-modal').click(function() {
   $('.info-modal').removeClass('close-info-modal');
   $('body').addClass('stop-scroll');
+  $('body').addClass('disable-scrolling');
 });
 
 // Close modal
 $('.info-modal-close').click(function() {
   $('.info-modal').addClass('close-info-modal');
   $('body').removeClass('stop-scroll');
+  $('body').removeClass('disable-scrolling');
 });
 
 // Sort movies by epoch date
@@ -33,3 +35,17 @@ for( var i in theatres ) {
   count.push(i);
 }
 $('#numOfTheatres').html(" " + count.length);
+
+document.ontouchmove = function ( event ) {
+  var isTouchMoveAllowed = true, target = event.target;
+  while ( target !== null ) {
+    if ( target.classList && target.classList.contains( 'disable-scrolling' ) ) {
+      isTouchMoveAllowed = false;
+      break;
+    }
+    target = target.parentNode;
+  }
+  if ( !isTouchMoveAllowed ) {
+    event.preventDefault();
+  }
+};
